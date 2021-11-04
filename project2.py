f3fe69817ea372b47086ba357499e8306640de0b
ELATHAM1 <89711321+ELATHAM1@users.noreply.github.com>
2021-11-04 15:51:11 -0500

    Update project2 to use regex for punctuation list

--- project2.py
+++ project2.py
@@ -1,9 +1,8 @@
 import nltk
 from nltk import *
+import re
 
-#CHAPTER 2 EXERCISE 18
-# Write a program to print the 50 most frequent bigrams (pairs of adjacent
-# words) of a text, omitting bigrams that contain stopwords.
+#CHAPTER 2 EXERCISE 18: Write a program to print the 50 most frequent bigrams (pairs of adjacent words) of a text, omitting bigrams that contain stopwords.
 
 from nltk.book import text2
 
@@ -15,21 +14,25 @@ stopwords = nltk.corpus.stopwords.words('english')
 allbigrams = bigrams(mytext)
 
 nonstopbigrams = [b for b in allbigrams if b[0] not in stopwords and b[1] not in stopwords]
-# check both elements of every bigram in allbigrams against the list of stopwords
-# b[0] indicates the first element, b[1] indicates the second
-# if neither element is in stopwords, add that bigram to the nonstopbigrams list
+    # check both elements of every bigram in allbigrams against the list of stopwords
+    # b[0] indicates the first element, b[1] indicates the second
+    # if neither element is in stopwords, add that bigram to the nonstopbigrams list
 
 bigramfreq = FreqDist(nonstopbigrams)
 print('The 50 most frequest bigrams with punctuation included are:', bigramfreq.most_common(50))
 
-#This way will exclude punctuation so we can see bigrams where both elements are actual words
+#The following code will exclude punctuation so we can see bigrams where both elements are actual words
 #I've commented it out because if both sets of code are active, the second FreqDist comes out empty
 
-# punctuation = ['.', '?', '"', '."', ',"', ',', "'", '-', '!', ';', ':']
-# #I'm sure there's a way to generate thish punctuation list with regex but we haven't talked about
-# #how to integrate it into python code yet
+# mystringtext = str(mytext)
 
-# allstops = stopwords + punctuation
+# punctuation = re.findall(r"""[,."!?:;-]+""", mystringtext, flags = re.I|re.M)
+# punct_set = list(set(punctuation))
+# punct_set.append("'")
+#     #Putting ' in the regex above didn't work -- characters like '.' and ',' were no longer expluded from the bigram results
+#     #I can see this having something to do with double quotes being used around ' in the text, but I don't fully understand why it had the effect it did
+
+# allstops = stopwords + punct_set
 
 # interestingbigrams = [p for p in allbigrams if p[0] not in allstops and p[1] not in allstops]
 # intbigramfreq = FreqDist(interestingbigrams)
@@ -37,10 +40,7 @@ print('The 50 most frequest bigrams with punctuation included are:', bigramfreq.
 
 
 
-#CHAPTER 2 EXERCISE 19
-#Write a program to create a table of word frequencies by genre, like the
-#one given in 1 for modals. Choose your own words and try to find words
-#whose presence (or absence) is typical of a genre. Discuss your findings.
+#CHAPTER 2 EXERCISE 19: Write a program to create a table of word frequencies by genre, like the one given in 1 for modals. Choose your own words and try to find words whose presence (or absence) is typical of a genre. Discuss your findings.
 
 from nltk.corpus import brown
 
